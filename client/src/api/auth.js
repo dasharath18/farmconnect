a665b9d3f0ab31596d78bd5c2820bf4cbeb21bc9
dasharath18 <dasharath1187@gmail.com>
2025-11-15 20:11:51 +0530

    Fix AddCropForm to use production API URL

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,25 +1,25 @@
-import axios from "axios";
-
-export const updateProfile = async (data) => 
-{
-  const res = await axios.put("http://localhost:5000/api/auth/me", data);
-  return res.data;
-};
 // import axios from "axios";
 
-// const API = process.env.REACT_APP_API_URL;
-
-// export const updateProfile = async (data) => {
-//   const res = await axios.put(`${API}/api/auth/me`, data);
+// export const updateProfile = async (data) => 
+// {
+//   const res = await axios.put("http://localhost:5000/api/auth/me", data);
 //   return res.data;
 // };
+import axios from "axios";
 
-// export const login = async (email, password) => {
-//   const res = await axios.post(`${API}/api/auth/login`, { email, password });
-//   return res.data;
-// };
+const API = process.env.REACT_APP_API_URL;
 
-// export const registerUser = async (payload) => {
-//   const res = await axios.post(`${API}/api/auth/register`, payload);
-//   return res.data;
-// };
+export const updateProfile = async (data) => {
+  const res = await axios.put(`${API}/api/auth/me`, data);
+  return res.data;
+};
+
+export const login = async (email, password) => {
+  const res = await axios.post(`${API}/api/auth/login`, { email, password });
+  return res.data;
+};
+
+export const registerUser = async (payload) => {
+  const res = await axios.post(`${API}/api/auth/register`, payload);
+  return res.data;
+};
--- client/src/api/wishlist.js
+++ client/src/api/wishlist.js
@@ -1,56 +1,5 @@
-import axios from "axios";
-const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
-
-const getAuthHeader = () => {
-  const token = localStorage.getItem("token");
-  return token ? { Authorization: `Bearer ${token}` } : {};
-};
-
-export const addToWishlist = async (cropId) => {
-  try {
-    const res = await axios.post(`${BASE}/api/wishlist`, { cropId }, {
-      headers: getAuthHeader(),
-    });
-    const wishlistItem = res.data;
-    window.dispatchEvent(new CustomEvent("wishlistChanged", {
-      detail: { action: "add", cropId, wishlistItem }
-    }));
-    return wishlistItem;
-  } catch (err) {
-    const msg = err.response?.data?.message || err.message || "Failed to add to wishlist";
-    throw new Error(msg);
-  }
-};
-
-export const removeFromWishlist = async (wishlistId) => {
-  try {
-    const res = await axios.delete(`${BASE}/api/wishlist/${wishlistId}`, {
-      headers: getAuthHeader()
-    });
-    window.dispatchEvent(new CustomEvent("wishlistChanged", {
-      detail: { action: "remove", wishlistId }
-    }));
-    return res.data;
-  } catch (err) {
-    const msg = err.response?.data?.message || err.message || "Failed to remove from wishlist";
-    throw new Error(msg);
-  }
-};
-
-export const getWishlist = async () => {
-  try {
-    const res = await axios.get(`${BASE}/api/wishlist`, {
-      headers: getAuthHeader()
-    });
-    return res.data;
-  } catch (err) {
-    const msg = err.response?.data?.message || err.message || "Failed to load wishlist";
-    throw new Error(msg);
-  }
-};
 // import axios from "axios";
-
-// const API = process.env.REACT_APP_API_URL;
+// const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 // const getAuthHeader = () => {
 //   const token = localStorage.getItem("token");
@@ -58,24 +7,75 @@ export const getWishlist = async () => {
 // };
 
 // export const addToWishlist = async (cropId) => {
-//   const res = await axios.post(
-//     `${API}/api/wishlist`,
-//     { cropId },
-//     { headers: getAuthHeader() }
-//   );
-//   return res.data;
+//   try {
+//     const res = await axios.post(`${BASE}/api/wishlist`, { cropId }, {
+//       headers: getAuthHeader(),
+//     });
+//     const wishlistItem = res.data;
+//     window.dispatchEvent(new CustomEvent("wishlistChanged", {
+//       detail: { action: "add", cropId, wishlistItem }
+//     }));
+//     return wishlistItem;
+//   } catch (err) {
+//     const msg = err.response?.data?.message || err.message || "Failed to add to wishlist";
+//     throw new Error(msg);
+//   }
 // };
 
 // export const removeFromWishlist = async (wishlistId) => {
-//   const res = await axios.delete(`${API}/api/wishlist/${wishlistId}`, {
-//     headers: getAuthHeader(),
-//   });
-//   return res.data;
+//   try {
+//     const res = await axios.delete(`${BASE}/api/wishlist/${wishlistId}`, {
+//       headers: getAuthHeader()
+//     });
+//     window.dispatchEvent(new CustomEvent("wishlistChanged", {
+//       detail: { action: "remove", wishlistId }
+//     }));
+//     return res.data;
+//   } catch (err) {
+//     const msg = err.response?.data?.message || err.message || "Failed to remove from wishlist";
+//     throw new Error(msg);
+//   }
 // };
 
 // export const getWishlist = async () => {
-//   const res = await axios.get(`${API}/api/wishlist`, {
-//     headers: getAuthHeader(),
-//   });
-//   return res.data;
+//   try {
+//     const res = await axios.get(`${BASE}/api/wishlist`, {
+//       headers: getAuthHeader()
+//     });
+//     return res.data;
+//   } catch (err) {
+//     const msg = err.response?.data?.message || err.message || "Failed to load wishlist";
+//     throw new Error(msg);
+//   }
 // };
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+
+const getAuthHeader = () => {
+  const token = localStorage.getItem("token");
+  return token ? { Authorization: `Bearer ${token}` } : {};
+};
+
+export const addToWishlist = async (cropId) => {
+  const res = await axios.post(
+    `${API}/api/wishlist`,
+    { cropId },
+    { headers: getAuthHeader() }
+  );
+  return res.data;
+};
+
+export const removeFromWishlist = async (wishlistId) => {
+  const res = await axios.delete(`${API}/api/wishlist/${wishlistId}`, {
+    headers: getAuthHeader(),
+  });
+  return res.data;
+};
+
+export const getWishlist = async () => {
+  const res = await axios.get(`${API}/api/wishlist`, {
+    headers: getAuthHeader(),
+  });
+  return res.data;
+};
