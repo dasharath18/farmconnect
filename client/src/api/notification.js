bfd7f0c08bf9c1dc43ebd89cb053bed1a2dcf937
dasharath18 <dasharath1187@gmail.com>
2025-11-15 20:06:47 +0530

    Fix AddCropForm to use production API URL

--- client/src/api/notification.js
+++ client/src/api/notification.js
@@ -1,3 +1,41 @@
+// import axios from "axios";
+// const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+// // Get all notifications
+// export const getNotifications = async (token) => {
+//   const res = await axios.get(`${BASE}/api/notifications`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// // MARK notification as READ
+// export const markNotificationRead = async (id, token) => {
+//   const res = await axios.put(
+//     `${BASE}/api/notifications/read/${id}`,
+//     {},
+//     {
+//       headers: { Authorization: `Bearer ${token}` },
+//     }
+//   );
+//   return res.data;
+// };
+
+// // DELETE notification (dismiss)
+// export const deleteNotification = async (id, token) => {
+//   const res = await axios.delete(`${BASE}/api/notifications/${id}`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// // Get unread notifications count
+// export const getUnreadCount = async (token) => {
+//   const res = await axios.get(`${BASE}/api/notifications/count`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
 import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL;
--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,57 +1,120 @@
 import axios from "axios";
+const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
-const API = process.env.REACT_APP_API_URL;
-
+// Create a new request
 export const createRequest = async (payload, token) => {
-  const res = await axios.post(`${API}/api/requests`, payload, {
+  const res = await axios.post(`${BASE}/api/requests`, payload, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
+// Get requests (all or mine=true)
 export const getRequests = async (query = {}, token) => {
   const params = new URLSearchParams(query).toString();
-  const url = `${API}/api/requests${params ? `?${params}` : ""}`;
-
+  const url = `${BASE}/api/requests${params ? `?${params}` : ""}`;
   const res = await axios.get(url, {
     headers: token ? { Authorization: `Bearer ${token}` } : {},
   });
   return res.data;
 };
 
+// Add customer request to farmer wishlist
 export const wishlistRequest = async (requestId, token) => {
   const res = await axios.post(
-    `${API}/api/requests/${requestId}/wishlist`,
+    `${BASE}/api/requests/${requestId}/wishlist`,
     {},
-    { headers: { Authorization: `Bearer ${token}` } }
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
   );
   return res.data;
 };
 
+// ---------------- NEW: Get single request by id ----------------
 export const getRequestById = async (id, token) => {
-  const res = await axios.get(`${API}/api/requests/${id}`, {
+  const res = await axios.get(`${BASE}/api/requests/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
+// ---------------- NEW: Update request ----------------
 export const updateRequest = async (id, payload, token) => {
-  const res = await axios.put(`${API}/api/requests/${id}`, payload, {
+  const res = await axios.put(`${BASE}/api/requests/${id}`, payload, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
+// ---------------- NEW: Delete request ----------------
 export const deleteRequest = async (id, token) => {
-  const res = await axios.delete(`${API}/api/requests/${id}`, {
+  const res = await axios.delete(`${BASE}/api/requests/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
 export const getFarmerWishlist = async (token) => {
-  const res = await axios.get(`${API}/api/requests/wishlist/farmer`, {
+  const res = await axios.get(`${BASE}/api/requests/wishlist/farmer`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
+// import axios from "axios";
+
+// const API = process.env.REACT_APP_API_URL;
+
+// export const createRequest = async (payload, token) => {
+//   const res = await axios.post(`${API}/api/requests`, payload, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// export const getRequests = async (query = {}, token) => {
+//   const params = new URLSearchParams(query).toString();
+//   const url = `${API}/api/requests${params ? `?${params}` : ""}`;
+
+//   const res = await axios.get(url, {
+//     headers: token ? { Authorization: `Bearer ${token}` } : {},
+//   });
+//   return res.data;
+// };
+
+// export const wishlistRequest = async (requestId, token) => {
+//   const res = await axios.post(
+//     `${API}/api/requests/${requestId}/wishlist`,
+//     {},
+//     { headers: { Authorization: `Bearer ${token}` } }
+//   );
+//   return res.data;
+// };
+
+// export const getRequestById = async (id, token) => {
+//   const res = await axios.get(`${API}/api/requests/${id}`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// export const updateRequest = async (id, payload, token) => {
+//   const res = await axios.put(`${API}/api/requests/${id}`, payload, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// export const deleteRequest = async (id, token) => {
+//   const res = await axios.delete(`${API}/api/requests/${id}`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
+
+// export const getFarmerWishlist = async (token) => {
+//   const res = await axios.get(`${API}/api/requests/wishlist/farmer`, {
+//     headers: { Authorization: `Bearer ${token}` },
+//   });
+//   return res.data;
+// };
